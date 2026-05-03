580877fa89c96b3e1ed5c6ebb0ea1c2142f8a92d
Sonali Sharma <74272170+Sonali-Sharma-tech@users.noreply.github.com>
2026-05-04 02:31:41 +0530

    security: replace hardcoded MongoDB URI with env variable

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors()); 
 
 app.use('/posts', postRoutes);
-const CONNECTION_URL = 'mongodb+srv://sonalisharmatech123:MyMongoDB123@cluster-memories.dcieo1h.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 4000; 
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
